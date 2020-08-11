54e64ca9ba323ad2d5afdcaec39160b6b0b60e91
nit29sin <nitish29singla@gmail.com>
2020-08-11 18:05:04 +0530

    Commit1

--- CICDDEVOPS/src/main/java/Myclass.java
+++ CICDDEVOPS/src/main/java/Myclass.java
@@ -3,7 +3,7 @@ public class Myclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jenkins 1");
+		System.out.println("Jenkins ");
 
 	}
 
